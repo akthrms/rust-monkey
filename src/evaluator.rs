b9eb87588bfb1cdd0620dcf5ac45bd445795644a
akthrms <akt.hrms@gmail.com>
2023-06-02 21:02:11 +0900

    feat: 単項・二項式を実装

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::{Expr, Program, Stmt},
+    ast::{Expr, Infix, Prefix, Program, Stmt},
     object::Object,
 };
 
@@ -31,7 +31,50 @@ impl Evaluator {
         match expr {
             Expr::Int(value) => Some(Object::Int(value)),
             Expr::Bool(value) => Some(Object::Bool(value)),
+            Expr::Prefix(prefix, right) => {
+                let right = self.eval_expr(*right)?;
+                self.eval_prefix_expr(prefix, right)
+            }
+            Expr::Infix(infix, left, right) => {
+                let left = self.eval_expr(*left)?;
+                let right = self.eval_expr(*right)?;
+                self.eval_infix_expr(infix, left, right)
+            }
             _ => unimplemented!(),
+        }
+    }
+
+    fn eval_prefix_expr(&mut self, prefix: Prefix, right: Object) -> Option<Object> {
+        match prefix {
+            Prefix::Bang => match right {
+                Object::Bool(value) => Some(Object::Bool(!value)),
+                Object::Null => Some(Object::Bool(true)),
+                _ => Some(Object::Bool(false)),
+            },
+            Prefix::Minus => match right {
+                Object::Int(value) => Some(Object::Int(-value)),
+                _ => Some(Object::Null),
+            },
+        }
+    }
+
+    fn eval_infix_expr(&mut self, infix: Infix, left: Object, right: Object) -> Option<Object> {
+        match (left, right) {
+            (Object::Int(left), Object::Int(right)) => match infix {
+                Infix::Plus => Some(Object::Int(left + right)),
+                Infix::Minus => Some(Object::Int(left - right)),
+                Infix::Asterisk => Some(Object::Int(left * right)),
+                Infix::Slash => Some(Object::Int(left / right)),
+                Infix::Lt => Some(Object::Bool(left < right)),
+                Infix::Gt => Some(Object::Bool(left > right)),
+                Infix::Eq => Some(Object::Bool(left == right)),
+                Infix::Ne => Some(Object::Bool(left != right)),
+            },
+            (left, right) => match infix {
+                Infix::Eq => Some(Object::Bool(left == right)),
+                Infix::Ne => Some(Object::Bool(left != right)),
+                _ => Some(Object::Null),
+            },
         }
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Object {
     Int(i64),
     Bool(bool),
